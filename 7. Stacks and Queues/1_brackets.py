dbd90f16e83a68c2c29fce7a4b737fb27eb9422f
Frederik <frederik.druyts@outlook.com>
2021-04-03 19:38:43 +0200

    Add cases

--- 7. Stacks and Queues/1_brackets.py
+++ 7. Stacks and Queues/1_brackets.py
@@ -9,6 +9,10 @@ def solution(S):
         if s in brackets:
             stack.append(s)
         else:
+            if len(stack) == 0:
+                return 0
             if not ((stack.pop(), s) in brackets.items()):
                 return 0
+    if len(stack) != 0:
+        return 0
     return 1
