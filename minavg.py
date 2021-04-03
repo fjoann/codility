fdda79d95330c782d26e49f20c872675a1945223
Frederik <frederik.druyts@outlook.com>
2021-04-03 14:40:38 +0200

    Fix indent

--- minavg.py
+++ minavg.py
@@ -5,11 +5,11 @@ def solution(A):
     min_avg = 10001
  
     for i in range(len(A) - 1):
-	    avg_2 = mean(A[i:i+1])
+        avg_2 = mean(A[i:i+2])
         if avg_2 < min_avg:
             min_i = i
             min_avg = avg_2
-		avg_3 = mean(A[i:i+2])
+        avg_3 = mean(A[i:i+3])
         if i < len(A) - 2 and avg_3 < min_avg:
             min_i = i
             min_avg = avg_3
