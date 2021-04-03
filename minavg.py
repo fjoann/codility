37dec42b39b6f70f0963a6fbad3061bb450c2f29
Frederik <frederik.druyts@outlook.com>
2021-04-03 14:27:43 +0200

    Rewrite entire solution

--- minavg.py
+++ minavg.py
@@ -1,17 +1,17 @@
 from statistics import mean
 
 def solution(A):
-    min_avg = mean(A[0:1])
-    min_idx = 0
-    for i in range(len(A) - 2):
-        avg_2 = mean(A[i:i+1])
-        avg_3 = mean(A[i:i+2])
+    min_i = 0
+    min_avg = 10001
+ 
+    for i in range(len(A) - 1):
+		avg_2 = mean(A[i:i+1])
         if avg_2 < min_avg:
+            min_i = i
             min_avg = avg_2
-            min_idx = i
-        if avg_3 < min_avg:
+		avg_3 = mean(A[i:i+2])
+        if i < len(A) - 2 and avg_3 < min_avg:
+            min_i = i
             min_avg = avg_3
-            min_idx = i
-    if mean(A[-2:]) < min_avg:
-        min_idx = len(A) - 2
-    return min_idx
+ 
+    return min_i
