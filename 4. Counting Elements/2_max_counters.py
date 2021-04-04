5d227f559dd21d6ee49f9dde119be8d20bd411cb
Frederik <frederik.druyts@outlook.com>
2021-04-04 17:00:50 +0200

    Update 2_max_counters.py

--- 4. Counting Elements/2_max_counters.py
+++ 4. Counting Elements/2_max_counters.py
@@ -6,7 +6,6 @@ def solution(N, A):
         if i == N + 1 and not last_max:
             C = [max_val] * N
             last_max = True
-            continue
         if i <= N:
             C[i - 1] += 1
             max_val = max(max_val, C[i - 1])
