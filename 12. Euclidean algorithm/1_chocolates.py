e190ce9fd62411e5c1322ee5ebc4d12aec622c75
Frederik <frederik.druyts@outlook.com>
2021-04-05 18:56:46 +0200

    Update 1_chocolates.py

--- 12. Euclidean algorithm/1_chocolates.py
+++ 12. Euclidean algorithm/1_chocolates.py
@@ -10,4 +10,11 @@ def lcm(a, b):
     return (a * b) // gcd(a, b)
 
 def solution(N, M):
+    """Calculate the number of eaten chocolates.
+    
+    N -- the number of chocolates in the circle
+    M -- the number of steps between each eaten chocolate
+    
+    N and M 'meet' at their lcm; it is the total number of steps taken.
+    """
     return lcm(N, M) // M
